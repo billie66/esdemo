bf564e6ad956494570f67c68ff2b8c6b1204b225
Billie Zhang <billiecoder@gmail.com>
2014-06-24 16:19:35 +0800

    highlight search keywords

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@ contents = [
 'Lorem ipsum dolor sit amet.',
 'Consectetur adipisicing elit, sed do eiusmod tempor incididunt.',
 'Labore et dolore magna aliqua.',
-'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-'Excepteur sint occaecat cupidatat non proident.'
+'Ut enim ad minim veniam, sed quis nostrud exercitation ullamco laboris.',
+'Excepteur sint occaecat cupidatat non proident sit amet.'
 ]
 
 puts "Deleting all users..."
 User.delete_all
 
-%w[ Tom Jack Kate Pete ].each_with_index do |name, i|
+%w[ Tom Jack Kate Pete dou].each_with_index do |name, i|
   User.create name: name, intro: contents[i]
 end
