91db269619c85174b9ad36848bfacdb29fc9c7c7
Billie Zhang <billiecoder@gmail.com>
2014-07-11 10:55:12 +0800

    use ik chinese analyzer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   include Elasticsearch::Model::Callbacks
 
   attr_accessible :intro, :name
+
+  settings index: { number_of_shards: 3 } do
+    mappings do
+      indexes :name,  type: 'string', index: "not_analyzed"
+      indexes :intro, type: 'string', analyzer: 'ik'
+    end
+ end
 end
